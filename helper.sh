d8f25e88f1ce74409ab07251b65f3a8824b38ab7
Cthulhu <ext@thepower.io>
2023-03-22 21:48:35 +0000

    Added firewall check

--- helper.sh
+++ helper.sh
@@ -11,11 +11,48 @@ NODECONFIG="/opt/thepower/node.config"
 CHECKURL="http://help.thepower.io:26299"
 
 HOSTNAME=$(grep hostname $NODECONFIG | cut -d '"' -f2)
+DNSIP=""
 
-if [ -z $HOSTNAME ]
-  then HOSTNAME="Hostname not defined"
+# FIREWALL
+echo -e "\033[35m\nCheck firewall ..."
+tput sgr0
+
+UFW=$(ufw status | grep -e "^Status")
+UFWCLOSED=""
+if [[ "$UFW" == *inactive* ]]
+  then echo -e "\033[32mFirewall is inactive"; tput sgr0
+  else echo -e "\033[33mFirewall is active"; tput sgr0
+       for P in "${PORTS[@]}"
+	 do
+           ufw status | grep -e "\b$P\b" | grep "ALLOW" > /dev/null 2>&1
+	   if [ "$?" -eq "0" ]
+	   then 
+	     echo -e "\033[32mPort ${PORTS[$P]} ($P) is open"
+	     tput sgr0
+           else
+	     echo -e "\033[31mPort ${PORTS[$P]} ($P) is closed  by firewall"
+	     tput sgr0
+	     UFWCLOSED="1"
+           fi
+	 done
 fi
 
+if [ "$UFWCLOSED" == "1" ]
+  then
+echo -e "
+\033[33mYou need to disable the firewall:
+    \033[34mufw disable"
+tput sgr0
+echo -e "
+\033[33mor open the ports:
+    \033[34mufw allow proto tcp from any to any port 80,1080,1443,1800 comment \"tpnode\"
+"
+  else echo -e "\033[32mGood !"
+fi
+tput sgr0
+
+# PORTS
+echo -e "\033[35m\nCheck ports ..."
 for P in "${PORTS[@]}"
   do
     PID=$(lsof -i :$P -t)
@@ -37,19 +74,35 @@ for P in "${!PORTS[@]}"
     PST["$P"]="$(echo $STATUS | jq -r .ports.$P)"
   done
 
-echo -e "\033[35m$HOSTNAME"
-echo -e "\033[34mip : $IP"
-
+PORTCLOSED=""
 for P in "${!PST[@]}"
   do
     if [ "${PST[$P]}" == "open" ]
-      then echo -e "\033[32m$P : ${PST[$P]}"
-      else echo -e "\033[31m$P : ${PST[$P]}"
+      then echo -e "\033[32mPort $P : ${PST[$P]}"
+      else echo -e "\033[31mPort $P : ${PST[$P]}"
+	   PORTCLOSED="1"
     fi
   done
 
+if [ -n "$PLIST" ]
+  then kill -15 $PLIST > /dev/null 2>&1
+fi
+
+if [ "$PORTCLOSED" == "1" ]
+  then echo -e "\033[31mYou need to open the ports"
+  else echo -e "\033[32mGood !"
+fi
 tput sgr0
 
-if [ -n "$PLIST" ]
-  then kill -9 $PLIST > /dev/null 2>&1
+# DNS
+echo -e "\033[35m\nCheck DNS ..."
+if [ -z $HOSTNAME ]
+  then echo -e "\033[31mHostname not defined !"; tput sgr0
+  else DNSIP=$(dig +short $HOSTNAME)
+echo -e "\033[35mHostname : $HOSTNAME"
+echo -e "\033[34m      ip : $IP"
+echo -e "\033[34m   dnsip : $DNSIP"
 fi
+
+
+tput sgr0
